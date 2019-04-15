4eeb2da88ae857fc5f074cbfbfd49130e88de54a
David Duong <david@duong.cz>
2019-04-15 13:41:41 +0200

    Add better rectangles

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/MapActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/MapActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,15 +26,12 @@ import java.util.ArrayList;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MapActivity extends Activity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
-    RelativeLayout map;
-    ListView list;
-
-    ArrayList<User> users = new ArrayList<>();
+    RecyclerView wrapList;
+    ArrayList<User> dataset = new ArrayList<>();
 
     UserAdapter adapter;
 
-    static final int MAX_SCORE = 16000;
+    static final int MAX_SCORE = 200;
     static final int MAX_SECTION = MAX_SCORE / 4;
 
     @Override
@@ -41,26 +39,16 @@ public class MapActivity extends Activity implements BottomNavigationView.OnNavi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        map = findViewById(R.id.map);
-        list = findViewById(R.id.user_list);
+        wrapList = findViewById(R.id.map_list);
 
+        dataset.add(new User("Ladislav Vagner", 0, getDrawable(R.drawable.avatar)));
+        dataset.add(new User("Jana Šetrná", MAX_SECTION + 2, getDrawable(R.drawable.avatar2)));
+        dataset.add(new User("Petr Přírodní", MAX_SECTION * 2 + 2, getDrawable(R.drawable.avatar3)));
+        dataset.add(new User("Petr Přírodní", MAX_SECTION * 3 + 2, getDrawable(R.drawable.avatar3)));
 
-        adapter = new UserAdapter(this, R.layout.list_item_user);
+        adapter = new UserAdapter(dataset);
 
-        users.add(new User("Ladislav Vagner", 0, getDrawable(R.drawable.avatar)));
-        users.add(new User("Jana Šetrná", MAX_SECTION + 2, getDrawable(R.drawable.avatar2)));
-        users.add(new User("Petr Přírodní", MAX_SECTION * 2 + 2, getDrawable(R.drawable.avatar3)));
-        users.add(new User("Petr Přírodní", MAX_SECTION * 3 + 2, getDrawable(R.drawable.avatar3)));
-
-        adapter.addAll(users);
-        list.setAdapter(adapter);
-
-
-        for (User user : users) {
-            addUserToMap(user);
-        }
-
-
+        wrapList.setAdapter(adapter);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
 
@@ -85,32 +73,7 @@ public class MapActivity extends Activity implements BottomNavigationView.OnNavi
         return true;
     }
 
-    void addUserToMap(User user) {
-        if (user.score <= MAX_SECTION) {
-            addImage(user.avatar, 50, 140);
-        } else if (user.score > MAX_SECTION && user.score <= MAX_SECTION * 2) {
-            if (user.score <= MAX_SECTION * 1.5) {
-                addImage(user.avatar, 98, 230);
-            } else {
-                addImage(user.avatar, 130, 293);
-            }
-        } else if (user.score > MAX_SECTION * 2 && user.score <= MAX_SECTION * 3) {
-            addImage(user.avatar, 220, 165);
-        } else if (user.score > MAX_SECTION * 3) {
-            addImage(user.avatar, 255, 220);
-        }
-    }
 
-    void addImage(Drawable drawable, int top, int left) {
-        CircleImageView testImage = new CircleImageView(this);
-
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(Math.round(convertDpToPixel(30, this)), Math.round(convertDpToPixel(30, this)));
-        params.leftMargin = (int) convertDpToPixel(left, this);
-        params.topMargin = (int) convertDpToPixel(top, this);
-
-        testImage.setImageDrawable(drawable);
-        map.addView(testImage, params);
-    }
 
     public static float convertDpToPixel(float dp, Context context){
         return dp * ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
@@ -132,33 +95,102 @@ public class MapActivity extends Activity implements BottomNavigationView.OnNavi
         }
     }
 
-    class UserAdapter extends ArrayAdapter<User> {
 
-        UserAdapter(@NonNull Context context, int resource) {
-            super(context, resource);
+    class UserAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        ArrayList<User> users;
+
+        public UserAdapter(ArrayList<User> users) {
+            this.users = users;
         }
+
+        class MapViewHolder extends RecyclerView.ViewHolder {
+            RelativeLayout map;
+            public MapViewHolder(@NonNull View itemView) {
+                super(itemView);
+                itemView.findViewById(R.id.map);
+            }
+
+            void addUserToMap(User user) {
+                if (user.score <= MAX_SECTION) {
+                    addImage(user.avatar, 50, 140);
+                } else if (user.score > MAX_SECTION && user.score <= MAX_SECTION * 2) {
+                    if (user.score <= MAX_SECTION * 1.5) {
+                        addImage(user.avatar, 98, 230);
+                    } else {
+                        addImage(user.avatar, 130, 293);
+                    }
+                } else if (user.score > MAX_SECTION * 2 && user.score <= MAX_SECTION * 3) {
+                    addImage(user.avatar, 220, 165);
+                } else if (user.score > MAX_SECTION * 3) {
+                    addImage(user.avatar, 255, 220);
+                }
+            }
+
+            void addImage(Drawable drawable, int top, int left) {
+                CircleImageView testImage = new CircleImageView(map.getContext());
+
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(Math.round(convertDpToPixel(30, map.getContext())), Math.round(convertDpToPixel(30, map.getContext())));
+                params.leftMargin = (int) convertDpToPixel(left, map.getContext());
+                params.topMargin = (int) convertDpToPixel(top, map.getContext());
+
+                testImage.setImageDrawable(drawable);
+                map.addView(testImage, params);
+            }
+        }
+
+        class ItemViewHolder extends RecyclerView.ViewHolder {
+            TextView text;
+            ImageView image;
+            TextView score;
+            public ItemViewHolder(@NonNull View itemView) {
+                super(itemView);
+                text = itemView.findViewById(R.id.user_name);
+                image = itemView.findViewById(R.id.user_image);
+                score = itemView.findViewById(R.id.user_score);
+            }
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+
+            if (position == 0) return 0;
+            return 1;
+        }
+
 
         @NonNull
         @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-            if (convertView == null) {
-                convertView = LayoutInflater.from(this.getContext()).inflate(R.layout.list_item_user, parent, false);
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int type) {
+            switch (type) {
+                case 0: return new MapViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_map, viewGroup, false));
+                default:
+                    return new ItemViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_user, viewGroup, false));
             }
+        }
 
-            User user = getItem(position);
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
 
-            if (user != null) {
-                TextView text = convertView.findViewById(R.id.user_name);
-                ImageView image = convertView.findViewById(R.id.user_image);
-                TextView score = convertView.findViewById(R.id.user_score);
+            if (getItemViewType(position) == 0) {
+                MapViewHolder holder = (MapViewHolder) viewHolder;
 
-                text.setText(user.username);
-                image.setImageDrawable(user.avatar);
-                score.setText(user.score.toString());
+                for (User user : users) {
+                    holder.addUserToMap(user);
+                }
+            } else {
+                ItemViewHolder holder = (ItemViewHolder) viewHolder;
+                User user = users.get(position - 1);
+                if (user != null) {
+                    holder.text.setText(user.username);
+                    holder.image.setImageDrawable(user.avatar);
+                    holder.score.setText(user.score.toString());
+                }
             }
+        }
 
-            return convertView;
+        @Override
+        public int getItemCount() {
+            return users.size() + 1;
         }
     }
 }
